250a68191b90836d6a23423b9e1f72436d4b5440
Markus Machel <machelmarkus@gmail.com>
2024-02-14 20:36:57 -0300

    faster approach

--- 1-TwoSum.cpp
+++ 1-TwoSum.cpp
@@ -1,17 +1,23 @@
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 class TwoSum {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int size = nums.size();
+        unordered_map<int, int> map;
 
-        for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        for (int i = 0;  i < nums.size(); i++) {
+            map[nums[i]] = i;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (map.find(complement) != map.end()) {
+                return {i, map[complement]};
             }
         }
+
+        return {};
     }
 };
